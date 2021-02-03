e397d9b730d658e356f5dae81493972e2e15325c
Jason L Perry <jason@suncoast.io>
2021-02-02 20:32:24 -0500

    Update
    A lazy commit.

--- ClassyClasses/Program.cs
+++ ClassyClasses/Program.cs
@@ -1,77 +1,50 @@
-﻿using System;
+using System;
 
 namespace ClassyClasses
 {
-
-  class Size
-  {
-    public int Length;
-    public int Width;
-
-    public Size(int l, int w) // <--- These are the parameters.
-    {
-      this.Length = l;
-      this.Width = w;
-    }
-  }
-
   class Point
   {
-    public int X;
-    public int Y;
+    int _SecretX;
+    int _SecretY;
+    int _AccessCounter;
 
     public Point(int x, int y)
     {
       this.X = x;
       this.Y = y;
     }
-  }
 
-  class Rect
-  {
-    public Point Origin { get; }
-    public Size Size { get; }
-
-    public Rect()
+    public int X
     {
-      this.Origin = new Point(0, 0);
-      this.Size = new Size(0, 0);
+      get
+      {
+        this._AccessCounter++;
+        return this._SecretX;
+      }
+      set
+      {
+        this._AccessCounter++;
+        this._SecretX = value;
+      }
     }
 
-    public Rect(Point origin, Size size)
+    public int Y
     {
-      this.Origin = origin;
-      this.Size = size;
+      get
+      {
+        this._AccessCounter++;
+        return this._SecretY;
+      }
+      set
+      {
+        this._AccessCounter++;
+        this._SecretY = value;
+      }
     }
 
-    public Rect(Point origin, int n)
+    public int AccessCount
     {
-      this.Origin = origin;
-      this.Size = new Size(n, n);
-    }
-
-     public Rect(int x, int y, int l, int w)
-    {
-      this.Origin = new Point(x, y);
-      this.Size = new Size(l, w);
-    }
-
-    public void Translate(int x, int y)
-    {
-      this.Origin.X += x;
-      this.Origin.Y += y;
-    }
-
-    public void Scale(int x, int y)
-    {
-      this.Size.Length *= x;
-      this.Size.Width *= y;
-    }
-
-    public void Scale(int m)
-    {
-      this.Size.Length *= m;
-      this.Size.Width *= m;
+      get => _AccessCounter;
     }
   }
 
@@ -79,34 +52,16 @@ namespace ClassyClasses
   {
     static void Main(string[] args)
     {
-      var point = new Point(0, 0); // Origin, {0, 0}
-      var size = new Size(4, 3);
-                     //    ^--- These are the arguments.
+      var point = new Point(10, 20); // Origin, {0, 0} <--
 
-      // point.X, point.Y
-      // size.Length, size.Width
-      
-      var oldTV = new Rect(point, size); // {0, 0}, {4, 3}
-      Console.WriteLine($"My old TV is {oldTV.Size.Length} by {oldTV.Size.Width}.");
+      Console.WriteLine($"X is {point.X}");
+      Console.WriteLine($"Y is {point.Y}");
 
-      var square = new Rect(point, 4);  // Square, {0, 0}, {4, 4}
-      Console.WriteLine($"My square is {square.Size.Length} by {square.Size.Width}.");
+      point.Y = 32; // <--
 
-      var newTV = new Rect(3, 4, 16, 10); // {3, 4} {19, 14}
-      Console.WriteLine($"My new TV is {newTV.Size.Length} by {newTV.Size.Width}.");
+      Console.WriteLine($"Y is {point.Y}");
 
-      Console.WriteLine($"My new TV at {newTV.Origin.X},{newTV.Origin.Y}.");
-      newTV.Translate(4, 10);
-
-      // newTV.Origin = new Point(4, 10);
-      Console.WriteLine($"My new TV now at {newTV.Origin.X},{newTV.Origin.Y}.");
-
-      newTV.Scale(2, 2);
-
-      Console.WriteLine($"My new TV is now {newTV.Size.Length} by {newTV.Size.Width}.");
-
-      newTV.Scale(5);
-      Console.WriteLine($"My new TV is now {newTV.Size.Length} by {newTV.Size.Width}.");
+      Console.WriteLine($"The point was accessed {point.AccessCount} times.");
     }
   }
 }
---
+++ ClassyClasses/ProgramOne.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace ClassyClasses
+{
+
+  class Size
+  {
+    public int Length;
+    public int Width;
+
+    public Size(int l, int w) // <--- These are the parameters.
+    {
+      this.Length = l;
+      this.Width = w;
+    }
+  }
+
+  class Point
+  {
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+      this.X = x;
+      this.Y = y;
+    }
+  }
+
+  class Rect
+  {
+    // public Point Origin;
+    Point _Origin;
+    Size _Size;
+
+    // Create an empty Rect
+    public Rect()
+    {
+      this._Origin = new Point(0, 0);
+      this._Size = new Size(0, 0);
+    }
+
+    // Create a Rect, with a given Point and Size
+    public Rect(Point origin, Size size)
+    {
+      this._Origin = origin;
+      this._Size = size;
+    }
+
+    // Create a Square
+    public Rect(Point origin, int n)
+    {
+      this._Origin = origin;
+      this._Size = new Size(n, n);
+    }
+
+    // Create an Rect given two coordinates (x and y) and two dimensions (length and width)
+     public Rect(int x, int y, int l, int w)
+    {
+      this._Origin = new Point(x, y);
+      this._Size = new Size(l, w);
+    }
+
+    public void Translate(int x, int y)
+    {
+      this._Origin.X += x;
+      this._Origin.Y += y;
+    }
+
+    public void Scale(int x, int y)
+    {
+      this._Size.Width *= x;
+      this._Size.Length *= y;
+    }
+
+    public void Scale(int m)
+    {
+      this._Size.Length *= m;
+      this._Size.Width *= m;
+    }
+
+    public Point Origin
+    {
+      get
+      {
+        return _Origin;
+      }
+
+      set
+      {
+        this._Origin = value;
+      }
+    }
+
+     public Size Size
+    {
+      get
+      {
+        return _Size;
+      }
+    }
+
+    public string Info
+    {
+      get
+      {
+        return $"LEFT: {this.Origin.X}, RIGHT: {this.Origin.X + this.Size.Width}, TOP: {this.Origin.Y + this.Size.Length}, BOTTOM: {this.Origin.Y}";
+      }
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var point = new Point(0, 0); // Origin, {0, 0}
+    }
+  }
+}
