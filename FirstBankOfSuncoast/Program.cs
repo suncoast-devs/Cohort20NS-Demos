90aa0deebd604768e0c12f5aaec1baf30cc2769d
Jason L Perry <jason@suncoast.io>
2021-02-18 20:26:34 -0500

    Update
    A lazy commit.

--- FirstBankOfSuncoast/Program.cs
+++ FirstBankOfSuncoast/Program.cs
@@ -1,46 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
 
 namespace FirstBankOfSuncoast
 {
   class Transaction
   {
-    public string Type;
-    public int Amount;
-    public string Account;
-
+    public string Type { get; set; }
+    public int Amount { get; set; }
+    public string Account { get; set; }
   }
 
   class Program
   {
+    static int PENNIES_PER_DOLLAR = 100;
     static List<Transaction> Transactions = new List<Transaction>();
     static void Main(string[] args)
     {
+      LoadTransactions();
+      var isRunning = true;
       // Main Menu
-      while (true)
+      while (isRunning)
       {
         Console.Clear();
         Console.WriteLine("1) Deposit to Checking");
-        Console.WriteLine("2) Withdrawl from Checking");
+        Console.WriteLine("2) Withdraw from Checking");
         Console.WriteLine("3) Show Checking Balance");
         Console.WriteLine("");
         Console.WriteLine("4) Deposit to Savings");
-        Console.WriteLine("5) Withdrawl from Savings");
+        Console.WriteLine("5) Withdraw from Savings");
         Console.WriteLine("6) Show Savings Balance");
 
         var option = GetIntegerFromUser("Choose an option (1-6) or 0 to quit.");
-
+    
         switch (option)
         {
             case 0:
               Console.WriteLine("Goodbye.");
+              isRunning = false;
               break;
             case 1:
               Deposit("Checking");
               break;
             case 2:
-              Withdrawl("Checking");
+              Withdraw("Checking");
               break;
             case 3:
               PrintBalance("Checking");
@@ -49,7 +55,7 @@ namespace FirstBankOfSuncoast
               Deposit("Savings");
               break;
             case 5:
-              Withdrawl("Savings");
+              Withdraw("Savings");
               break;
             case 6:
               PrintBalance("Savings");
@@ -59,8 +65,6 @@ namespace FirstBankOfSuncoast
               Console.ReadKey();
               break;
         }
-
-        if (option == 0 ) break;
       }
     }
 
@@ -68,36 +72,57 @@ namespace FirstBankOfSuncoast
     {
       Console.Clear();
       Console.WriteLine($"Deposit into {account}");
-      var amount = GetMoneyFromUser("How much do you want to deposit?");
+      var amount = GetMoneyFromUser ("How much do you want to deposit?");
       var transaction = new Transaction() {
         Type = "Deposit",
         Account = account,
         Amount = amount,
       };
       Transactions.Add(transaction);
-      // TODO: Write to file.
+      PersistTransactions();
       PrintBalance(account);
     }
 
-    static void Withdrawl(string accountType)
+    static void Withdraw(string account)
     {
       Console.Clear();
-      Console.WriteLine($"Withdrawl from {accountType}");
+      Console.WriteLine($"Withdraw into {account}");
+      var amount = GetMoneyFromUser("How much do you want to withdraw?");
+      var transaction = new Transaction() {
+        Type = "Withdraw",
+        Account = account,
+        Amount = amount,
+      };
 
-      // TODO: Implement Withdrawl
-      Console.ReadKey();
+      // TODO: Ensure Balance is gte the transaction amount.
+      if (GetBalance(account) >= transaction.Amount)
+      {
+        Transactions.Add(transaction);
+        PersistTransactions();
+      } else {
+        Console.WriteLine("Insufficient Funds.");
+      }
+      PrintBalance(account);
     }
 
     static void PrintBalance(string account)
     {
-      // TODO: Implement Balance Correctly
-      var depositTotal = Transactions
-        .Where(t => t.Account == account)
-        .Sum(t => t.Amount);
-      Console.Clear();
-      Console.WriteLine($"Balance of {account}: {depositTotal}");
-      Console.WriteLine("Press any key to continue...");
+      Console.WriteLine($"Balance of {account}: {FormatMoney(GetBalance(account))}");
+      Console.Write("\nPress any key to continue...");
       Console.ReadKey();
+    }
+
+    static int GetBalance(string account)
+    {
+      var depositTotal = Transactions
+        .Where(t => t.Type == "Deposit" && t.Account == account)
+        .Sum(t => t.Amount);
+
+      var withdrawTotal = Transactions
+        .Where(t => t.Type == "Withdraw" && t.Account == account)
+        .Sum(t => t.Amount);
+
+      return depositTotal - withdrawTotal;
     }
 
     static int GetIntegerFromUser(string message)
@@ -125,7 +150,36 @@ namespace FirstBankOfSuncoast
           input = Console.ReadLine();
         } while(!float.TryParse(input, out valueInDollars));
 
-        return Convert.ToInt32(valueInDollars * 100);
+        return Convert.ToInt32(valueInDollars * PENNIES_PER_DOLLAR);
     }
-  }
+
+    // Given a number like 4200, returns a string like $42.00
+    static string FormatMoney(int amountInCents)
+    {
+      double amountInDollars = Convert.ToDouble(amountInCents) / PENNIES_PER_DOLLAR;
+      return amountInDollars.ToString("C2");
+    }
+   static void PersistTransactions()
+    {
+      var fileWriter = new StreamWriter("transactions.csv");
+      var csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture);
+      csvWriter.WriteRecords(Transactions);
+      fileWriter.Close();
+    }
+
+    static void LoadTransactions()
+    {
+      try
+      {
+        var fileReader = new StreamReader("transactions.csv");
+        var csvReader = new CsvReader(fileReader, CultureInfo.InvariantCulture);
+        Transactions = csvReader.GetRecords<Transaction>().ToList();
+        fileReader.Close();          
+      }
+      catch (System.IO.FileNotFoundException)
+      {
+
+      }
+    }
+  } 
 }
